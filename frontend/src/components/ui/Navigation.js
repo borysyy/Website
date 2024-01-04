880a38d442d440e3811ebfb449e55a304cfaf6c4
borysyy <borysyy@sunypoly.edu>
2024-01-04 00:27:58 -0500

    More nav changes

--- frontend/src/components/ui/Navigation.js
+++ frontend/src/components/ui/Navigation.js
@@ -1,7 +1,8 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import React from 'react';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
 
 function Navigation() {
   return (
@@ -11,16 +12,15 @@ function Navigation() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link className="d-lg-none" href="/">Home</Nav.Link>
-            <Nav.Link href="/education">Education</Nav.Link>
-            <Nav.Link href="/experience">Experience</Nav.Link>
-            <Nav.Link href="/contact">Contact</Nav.Link>
+            <Nav.Link as={Link} to="/" className="d-lg-none">Home</Nav.Link>
+            <Nav.Link as={Link} to="/education">Education</Nav.Link>
+            <Nav.Link as={Link} to="/experience">Experience</Nav.Link>
+            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
-    
       </Container>
     </Navbar>
-  );
+  )
 }
 
-export default Navigation;
+export default Navigation
