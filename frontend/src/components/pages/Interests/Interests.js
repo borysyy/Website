daac6ada0a51c70c066f00fa7b6b6e5ab6e19bb7
Slava <borysyy@sunypoly.edu>
2024-01-11 20:16:34 -0500

    New style changes

--- frontend/src/components/pages/Interests/Interests.js
+++ frontend/src/components/pages/Interests/Interests.js
@@ -1,15 +1,13 @@
 import React from 'react'
-import Cards from '../..//ui/Interests/Cards'
+import Cards from '../../ui/Interests/Cards'
 
 const Interests = () => {
   return (
     <div>
-      <div className="interests-header px-3 py-3 mx-auto text-center">
+      <div className="interests-header">
         <h1 className="display-4">My hobbies</h1>
         <p className="lead">
-          Quickly build an effective pricing table for your potential customers
-          with this Bootstrap example. It's built with default Bootstrap
-          components and utilities with little customization.
+          Ranging from computer science related topics, to TV shows and movies.
         </p>
       </div>
       <div>
